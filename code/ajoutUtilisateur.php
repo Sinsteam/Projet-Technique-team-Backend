551ae49f01ca935f1856e12e19c5ea014bcbfc99
Sins_Luxuria <agamer49000@gmail.com>
2022-12-09 14:07:40 +0100

    envoi de donnée via classe utilisateur sans fonction

--- code/ajoutUtilisateur.php
+++ code/ajoutUtilisateur.php
@@ -2,33 +2,33 @@
 include_once "Classe/Utilisateur.Class.php";
 include_once "config.php";
 
-echo "Bonjour";
 
+$ajoutUtilisateur = new Utilisateur(filter_input(INPUT_POST, "mail"),
+    filter_input(INPUT_POST, "type"),
+    filter_input(INPUT_POST, "profil"),
+    filter_input(INPUT_POST, "nom"),
+    filter_input(INPUT_POST, "prenom"),
+    filter_input(INPUT_POST, "promo"),
+    filter_input(INPUT_POST, "motdepasse"));
+echo "coucou";
 
+//on mets les valeurs de l'objet récupéré dans des variables pour préparer l'envoi
+$mail = $ajoutUtilisateur->getMail();
+$type = $ajoutUtilisateur->getType();
+$profil = $ajoutUtilisateur->getProfile();
+$nom = $ajoutUtilisateur->getNom();
+$prenom  = $ajoutUtilisateur->getPrenom();
+$promotion = $ajoutUtilisateur->getPromotion();
+$password = password_hash($ajoutUtilisateur->getPwd(),PASSWORD_DEFAULT);
+echo "$password";
 
-
-$fonction = filter_input(INPUT_POST,"functionname");
-echo "$fonction";
-function ajoutUser()
-{
-    $ajoutUtilisateur = new Utilisateur(filter_input(INPUT_POST, "mail"),
-        filter_input(INPUT_POST, "type"),
-        filter_input(INPUT_POST, "profil"),
-        filter_input(INPUT_POST, "nom"),
-        filter_input(INPUT_POST, "prenom"),
-        filter_input(INPUT_POST, "promo"),
-        filter_input(INPUT_POST, "motdepasse"));
-    echo "coucou";
-
-
-    $pdo = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=" . Config::BDD, Config::UTILISATEUR, Config::MOTDEPASSE);
-    $req = $pdo->prepare("insert into utilisateur (Id_email,type,profil,nom,prenom,promotion,password) values (:id_email,:type,:profil,:nom,:prenom,:promotion,:password)");
-    $req->bindParam(":id_email", $id_email);
-    $req->bindParam(":type", $type);
-    $req->bindParam(":profil", $profil);
-    $req->bindParam(":nom", $nom);
-    $req->bindParam(":prenom", $prenom);
-    $req->bindParam(":promotion", $promotion);
-    $req->bindParam(":password", $password);
-    $req->execute();
-}
+$pdo = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=" . Config::BDD, Config::UTILISATEUR, Config::MOTDEPASSE);
+$req = $pdo->prepare("insert into utilisateur (Id_email,type,profil,nom,prenom,promotion,password) values (:id_email,:type,:profil,:nom,:prenom,:promotion,:password)");
+$req->bindParam(":id_email", $mail);
+$req->bindParam(":type", $type);
+$req->bindParam(":profil", $profil);
+$req->bindParam(":nom", $nom);
+$req->bindParam(":prenom", $prenom);
+$req->bindParam(":promotion", $promotion);
+$req->bindParam(":password", $password);
+$req->execute();
