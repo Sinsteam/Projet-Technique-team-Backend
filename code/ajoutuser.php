4b7f6b4c5e9e87f0cdbfbe1e52b2272ae257b115
Sins_Luxuria <agamer49000@gmail.com>
2022-12-09 15:48:16 +0100

    envoi de donnée via classe utilisateur avec fonction

--- code/ajoutuser.php
+++ code/ajoutuser.php
@@ -1,9 +1,7 @@
 <?php
 include_once "ajoutUtilisateur.php";
 if(array_key_exists('submit', $_POST)){
-    $user = new Utilisateur(filter_input(INPUT_POST, "mail"),
-        filter_input(INPUT_POST, "type"),
-        filter_input(INPUT_POST, "profil"),
+    $user = new Utilisateur(filter_input(INPUT_POST, "mail"), 0,0,
         filter_input(INPUT_POST, "nom"),
         filter_input(INPUT_POST, "prenom"),
         filter_input(INPUT_POST, "promo"),
@@ -32,14 +30,6 @@ if(array_key_exists('submit', $_POST)){
         <input type="email" id="mail" name="mail">
     </div>
     <div>
-        <label for="type">Type :</label>
-        <input type="number" id="type" name="type">
-    </div>
-    <div>
-        <label for="profil">profil :</label>
-        <input type="number" id="profil" name="profil">
-    </div>
-    <div>
         <label for="promo">Vous venez de quelle promo ?</label>
         <select id="promo" name="promo">
             <option value="Bachelor" selected>Bachelor</option>
@@ -56,37 +46,3 @@ if(array_key_exists('submit', $_POST)){
 </form>
 </body>
 </html>
-<script>
-    /*function sendJSON(){
-        let nom = document.querySelector('#nom');
-        let mail = document.querySelector('#mail');
-        let prenom = document.querySelector('#prenom');
-        let type = document.querySelector('#type');
-        let profil = document.querySelector('#profil');
-        let promo = document.querySelector('#promo').value;
-        let mdp = document.querySelector('#mdp');
-
-        // Creating a XHR object
-        let xhr = new XMLHttpRequest();
-        let url = "recupJson.php";
-
-        // open a connection
-        xhr.open("POST", url, true);
-
-        // Set the request header i.e. which type of content you are sending
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        // Converting JSON data to string
-        var data = {
-            "mail": mail.value,
-            "type": type.value,
-            "profil":profil.value,
-            "nom": nom.value,
-            "prenom": prenom.value,
-            "promo":promo.value,
-            "mdp":mdp.value
-        };
-        // Sending data with the request
-        xhr.send(data);
-    }*/
-</script>
